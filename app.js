9c4705657837a39be28663f13f1b56ac4fc4bbbc
yanagi-kk <72825305+yanagi-kk@users.noreply.github.com>
2022-02-08 18:07:24 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');  //MySQLを使う用
 const session = require("express-session"); //セッションする用
 const bcrypt = require('bcrypt'); //パスワード暗号化用
+const res = require('express/lib/response');
 const app = express();
 
 app.use(express.static('public'));
@@ -14,7 +15,6 @@ const connection = mysql.createConnection({
   database: 'Koasuky'
 });
 
-/*
 app.use(
     session({
       secret: 'my_secret_key',
@@ -22,7 +22,6 @@ app.use(
       saveUninitialized: false,
     })
 );
-*/
 
 //ログインの状態を毎回確認します
 /*
@@ -55,25 +54,36 @@ app.get('/', (req, res) => {
 
 
 // 内容画面に対応するルーティングです
-/*
 app.get('/content/:id', (req, res) => {
-  const id = req.params.id;
+  const itemId = req.params.id; //ルートパラメータから取得したidをitemIdへ
   connection.query(
     'SELECT * FROM clothes WHERE id = ?',
-    [id],
+    [itemId],
     (error, results) => {
-      res.render('content.ejs', { cloth: results[0] });
-    }
-  );
+      console.log(results);
+      res.render('content.ejs', { item: results[0] });
+    });
 });
-*/
+
+//カートに入れるボタンを押したときに対応するルーティングです
+app.post("/add", (req, res) => {
+  const itemId = req.params.id;
+  const userId = req.session.userId;
+  connection.query(
+    "INSERT INTO customers (cart) VALUES (?) WHERE id = ?", //お客のIDからcart情報を取得
+    [itemId, userId],
+    (error, results) => {
+      console.log("itemId: %d",itemId);
+      res.redirect("/content/:id");
+    });
+});
 
 // アバウト画面に対応するルーティングです
-/*
 app.get('/about', (req, res) => {
   res.render('about.ejs');
 });
 
+/*
 //ログイン画面に対応するルーティングです
 app.get('/login', (req, res) => {
     connection.query(
@@ -100,30 +110,64 @@ app.get("/signup", (req, res) => {
 */
 
 //カート画面に対応するルーティングです
-/*
 app.get("/cart", (req, res) => {
+  const userId = req.session.userId;
   connection.query(
-    'INSERT INTO customers (purchased_count) VALUES (?)',
-    [purchased_count],
+    'SELECT cart FROM customers WHERE id = ?',
+    [userId],
     (error, results) => {
-      res.render('cart.ejs', { customers: results });
+      res.render('cart.ejs', { carts: results });
     }
   );
 }); 
-*/
+
+app.post("/purchase", (req, res) => {
+  const userId = req.session.userId;
+  const purchase_counts[] = req.body.purchase_count; //今回お客が買う商品のそれぞれの個数(配列にする)
+  var counter = 0; //何回繰り返しているか数える用
+  purchase_counts.forEach((count) => {
+    counter++;
+    connection.query(
+      "UPDATE clothes SET purchased_count = purchased_count + ? WHERE id = ( SELECT cart FROM customers WHERE id = ?)",
+      // 上のSQLでは、データベースclothesのpurchased_countの値をお客が買った分だけ増やす（未完成）
+      [count, userId],
+      (error_p, results_p) => {
+        if(counter == purchase_counts.length-1){ //最後の繰り返しなら、cartの中身を空（NULL）にしてHOMEに戻る
+        connection.query(
+          "UPDATE customers SET cart = NULL WHERE id = ?"
+          [userId],
+          (error_c, results_c) => {
+            res.redirect("/purchased");
+          });
+        }
+      });
+  });
+});
 
 //購入完了画面に対応するルーティングです
-/*
 app.get("/purchased", (req, res) => {
   res.render("purchased.ejs");
 }); 
-*/
 
 // 問い合わせ画面に対応するルーティングです
-/*
 app.get('/contact', (req, res) => {
   res.render('contact.ejs');
 });
-*/
+
+//問い合わせ画面で送信ボタンを押した時に対応するルーティングです
+app.post("/contact_submit", (req, res) => {
+  const name = req.body.name; 
+  const email = req.body.email;
+  const title = req.body.title;
+  const contents = req.body.contents;
+
+  connection.query(
+    "INSERT INTO contacts (name, email, title contents) VALUES (?, ?, ?, ?)",
+    [name, email, title, contents],
+    (error, results) => {
+      res.redirect("/");
+    }
+  );
+});
 
 app.listen(3000);
